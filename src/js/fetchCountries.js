31d24a28fedb68ef81efe83e0a6d77076857ec21
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-04-20 11:15:15 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,11 +4,11 @@
    fetchCountries(name) {
     const queryString = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
    return fetch(queryString).then(r => 
-    {
+    
       if (!resp.ok) {
         throw new Error(response.statusText)
     }
-    }
+  
     r.json());
 }
 }
