a438da7157f6ba1189785125b76a1183d772dd2a
VladimirPopovich27 <117470833+VladimirPopovich27@users.noreply.github.com>
2023-04-20 21:01:58 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -6,14 +6,29 @@
 // }
 // }
 
+// export default class ApiService {
+//   constructor() {}
+
+//   fetchCountries(name) {
+//     const queryString = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+//     if (!resp.ok) {
+//      throw new Error(response.statusText)
+//     }
+//     return fetch(queryString).then((r) => r.json());
+//   }
+// }
+
 export default class ApiService {
   constructor() {}
 
   fetchCountries(name) {
     const queryString = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-    // if (!resp.ok) {
-    //   throw new Error(response.statusText)
-    // }
-    return fetch(queryString).then((r) => r.json());
+    return fetch(queryString)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error(resp.statusText);
+        }
+        return resp.json();
+      })
   }
 }
